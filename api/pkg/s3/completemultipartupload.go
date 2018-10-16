9b53f8b78a2b0a0ddb0711f1d19ec6d5a79b9ea5
liuqinguestc <liuqinguestc@163.com>
2018-10-16 09:43:37 +0800

    add multipartupload func

---
+++ api/pkg/s3/completemultipartupload.go
@@ -0,0 +1,42 @@
+package s3
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/emicklei/go-restful"
+
+	//	"github.com/micro/go-micro/errors"
+
+	. "github.com/opensds/multi-cloud/s3/pkg/exception"
+	s3 "github.com/opensds/multi-cloud/s3/proto"
+)
+
+func (s *APIService) CompleteMultipartUpload(request *restful.Request, response *restful.Response) {
+	bucketName := request.PathParameter("bucketName")
+	objectKey := request.PathParameter("objectKey")
+	UploadId := request.PathParameter("uploadId")
+	contentLenght := request.HeaderParameter("content-length")
+	ctx := context.WithValue(request.Request.Context(), "operation", "MultipartUpload")
+	object := s3.Object{}
+	object.ObjectKey = objectKey
+	object.BucketName = bucketName
+	multipartUpload := s3.MultipartUpload{}
+	multipartUpload.Bucket = bucketName
+	multipartUpload.Key = objectKey
+	multipartUpload.UploadId = UploadId
+	size, _ := strconv.ParseInt(contentLenght, 10, 64)
+	object.Size = size
+
+	client := _getBackendClient(s, bucketName)
+	if client == nil {
+		response.WriteError(http.StatusInternalServerError, NoSuchBackend.Error())
+		return
+	}
+	s3err := client.COMPLETEMULTIPARTUPLOAD(&multipartUpload, ctx)
+	if s3err != NoError {
+		response.WriteError(http.StatusInternalServerError, s3err.Error())
+		return
+	}
+}
---
+++ api/pkg/s3/multipartupload.go
@@ -0,0 +1,64 @@
+// Copyright (c) 2018 Huawei Technologies Co., Ltd. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package s3
+
+import (
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	"github.com/micro/go-log"
+
+	//	"github.com/micro/go-micro/errors"
+	"strconv"
+
+	. "github.com/opensds/multi-cloud/s3/pkg/exception"
+	"github.com/opensds/multi-cloud/s3/proto"
+	"golang.org/x/net/context"
+)
+
+//ObjectPut -
+func (s *APIService) MultiPartUploadInit(request *restful.Request, response *restful.Response) {
+	bucketName := request.PathParameter("bucketName")
+	objectKey := request.PathParameter("objectKey")
+	UploadId := request.PathParameter("UploadId")
+	contentLenght := request.HeaderParameter("content-length")
+
+	ctx := context.WithValue(request.Request.Context(), "operation", "multiupload")
+
+	log.Logf("Received request for create bucket: %s", bucketName)
+
+	object := s3.Object{}
+	object.ObjectKey = objectKey
+	object.BucketName = bucketName
+	multipartUpload := s3.MultipartUpload{}
+	multipartUpload.Bucket = bucketName
+	multipartUpload.Key = objectKey
+	multipartUpload.UploadId = UploadId
+	size, _ := strconv.ParseInt(contentLenght, 10, 64)
+	object.Size = size
+
+	client := _getBackendClient(s, bucketName)
+	if client == nil {
+		response.WriteError(http.StatusInternalServerError, NoSuchBackend.Error())
+		return
+	}
+	res, s3err := client.INITMULTIPARTUPLOAD(&object, ctx)
+	if s3err != NoError {
+		response.WriteError(http.StatusInternalServerError, s3err.Error())
+		return
+	}
+	log.Log("Uploadpart successfully.")
+	response.WriteEntity(res)
+}
