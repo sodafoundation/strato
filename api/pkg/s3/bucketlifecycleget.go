f7b0a013295d7732caf99b41ac13653eb1196660
nguptaopensds <ngopensds@gmail.com>
2019-05-15 19:15:37 +0530

    Addressed comments for GET Lifecycle API

--- api/pkg/s3/bucketlifecycleget.go
+++ api/pkg/s3/bucketlifecycleget.go
@@ -15,14 +15,44 @@
 package s3
 
 import (
+	"fmt"
+
 	"github.com/emicklei/go-restful"
 	"github.com/micro/go-log"
 	"github.com/opensds/multi-cloud/api/pkg/policy"
+	. "github.com/opensds/multi-cloud/api/pkg/utils/constants"
 	"github.com/opensds/multi-cloud/s3/pkg/model"
 	s3 "github.com/opensds/multi-cloud/s3/proto"
 	"golang.org/x/net/context"
 )
 
+//Convert function from storage tier to storage class for XML format output
+func (s *APIService) tier2class(tier int32) (string, error) {
+	{
+		mutext.Lock()
+		defer mutext.Unlock()
+		if len(ClassAndTier) == 0 {
+			err := s.loadStorageClassDefinition()
+			if err != nil {
+				log.Logf("load storage classes failed: %v.\n", err)
+				return "", err
+			}
+		}
+	}
+	className := ""
+	for k, v := range ClassAndTier {
+		if v == tier {
+			className = k
+		}
+	}
+	if className == "" {
+		log.Logf("invalid tier: %d\n", tier)
+		return "", fmt.Errorf(InvalidTier)
+	}
+	return className, nil
+}
+
+//Function for GET Bucket Lifecycle API
 func (s *APIService) BucketLifecycleGet(request *restful.Request, response *restful.Response) {
 	if !policy.Authorize(request, response, "bucket:get") {
 		return
@@ -35,47 +65,60 @@ func (s *APIService) BucketLifecycleGet(request *restful.Request, response *rest
 
 	// convert back to xml struct
 	getLifecycleConf := model.LifecycleConfiguration{}
-	getLifecycleConf.Rule = make([]model.Rule, 0)
-	// convert lifecycle rule to xml Rule
-	for _, lcRule := range bucket.LifecycleConfiguration {
-		xmlRule := model.Rule{}
 
-		xmlRule.Status = lcRule.Status
-		xmlRule.ID = lcRule.ID
-		xmlRule.Filter = converts3FilterToRuleFilter(lcRule.Filter)
-		xmlRule.AbortIncompleteMultipartUpload = converts3UploadToRuleUpload(lcRule.AbortIncompleteMultipartUpload)
-		xmlRule.Transition = make([]model.Transition, 0)
-		for _, transition := range lcRule.Actions {
-			xmlTransition := model.Transition{}
-			xmlTransition.Days = transition.Days
-			//xmlTransition.StorageClass = transition.Tier
-			xmlTransition.Backend = transition.Backend
-			xmlRule.Transition = append(xmlRule.Transition, xmlTransition)
+	// convert lifecycle rule to xml Rule
+	if bucket.LifecycleConfiguration != nil {
+		for _, lcRule := range bucket.LifecycleConfiguration {
+			xmlRule := model.Rule{}
+
+			xmlRule.Status = lcRule.Status
+			xmlRule.ID = lcRule.ID
+			xmlRule.Filter = converts3FilterToRuleFilter(lcRule.Filter)
+			xmlRule.AbortIncompleteMultipartUpload = converts3UploadToRuleUpload(lcRule.AbortIncompleteMultipartUpload)
+			xmlRule.Transition = make([]model.Transition, 0)
+
+			//Arranging the transition and expiration actions in XML
+			for _, action := range lcRule.Actions {
+				log.Logf("Action is : %v\n", action)
+
+				if action.Name == ActionNameTransition {
+					xmlTransition := model.Transition{}
+					xmlTransition.Days = action.Days
+					xmlTransition.Backend = action.Backend
+					className, err := s.tier2class(action.Tier)
+					if err == nil {
+						xmlTransition.StorageClass = className
+					}
+					xmlRule.Transition = append(xmlRule.Transition, xmlTransition)
+				}
+				if action.Name == ActionNameExpiration {
+					xmlExpiration := model.Expiration{}
+					xmlExpiration.Days = action.Days
+					xmlRule.Expiration = append(xmlRule.Expiration, xmlExpiration)
+				}
+			}
+			// append each xml rule to xml array
+			getLifecycleConf.Rule = append(getLifecycleConf.Rule, xmlRule)
 		}
-		for _, expiration := range lcRule.Actions {
-			xmlExpiration := model.Expiration{}
-			xmlExpiration.Days = expiration.Days
-			xmlRule.Expiration = append(xmlRule.Expiration, xmlExpiration)
-		}
-		// append each xml rule to xml array
-		getLifecycleConf.Rule = append(getLifecycleConf.Rule, xmlRule)
 	}
 
-	// marshall the array back to xml
-	//xmlByteArr, _ := xml.Marshal(getLifecycleConf.Rule)
-	response.WriteAsXml(getLifecycleConf.Rule)
-
+	// marshall the array back to xml format
+	response.WriteAsXml(getLifecycleConf)
 	log.Log("Get bucket lifecycle successfully.")
 }
 
 func converts3FilterToRuleFilter(filter *s3.LifecycleFilter) model.Filter {
 	retFilter := model.Filter{}
-	retFilter.Prefix = filter.Prefix
+	if filter != nil {
+		retFilter.Prefix = filter.Prefix
+	}
 	return retFilter
 }
 
 func converts3UploadToRuleUpload(upload *s3.AbortMultipartUpload) model.AbortIncompleteMultipartUpload {
 	retUpload := model.AbortIncompleteMultipartUpload{}
-	retUpload.DaysAfterInitiation = upload.DaysAfterInitiation
+	if upload != nil {
+		retUpload.DaysAfterInitiation = upload.DaysAfterInitiation
+	}
 	return retUpload
 }
--- api/pkg/s3/lifecycleerrorcodes.go
+++ api/pkg/s3/lifecycleerrorcodes.go
@@ -22,4 +22,5 @@ const (
 	DaysInStorageClassBeforeTransition = "error: minimum days for an object in the current storage class is less before transition action"
 	NoRuleIDForLifecycleDelete = "error: No rule ID specified to delete from lifecycle configuration"
 	WrongRuleIDForLifecycleDelete = "error: The rule ID which is specified for delete does not exist"
+	InvalidTier = "error: Invalid tier"
 )
