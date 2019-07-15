3674bebfab4c1ea600a82ac277844b33334d6223
Satya.G <satyang@huawei.com>
2019-07-15 14:52:34 -0700

    Test automation for data lifecycle

--- api/pkg/s3/bucketlifecycledelete_test.go
+++ api/pkg/s3/bucketlifecycledelete_test.go
@@ -27,31 +27,80 @@
 package s3
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/emicklei/go-restful"
+	"github.com/micro/go-log"
 	backend "github.com/opensds/multi-cloud/backend/proto"
+	backendMock "github.com/opensds/multi-cloud/backend/proto/mocks"
 	s3 "github.com/opensds/multi-cloud/s3/proto"
+	"github.com/opensds/multi-cloud/s3/proto/mocks"
+	"golang.org/x/net/context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
-func TestAPIService_BucketLifecycleDelete(t *testing.T) {
+
+
+var (
+	Result1 string
+	tx     *testing.T
+)
+
+func BucketDelHandler(rq *restful.Request, rp *restful.Response) {
+
+	type dumb struct {
+	}
+	dmbs := dumb{}
+	s3Resp := s3.BaseResponse{"200", "success", dmbs, nil, 0}
+	s3ClientMock := &mocks.S3Service{}
+	backEndMock := &backendMock.BackendService{}
+	s3ClientMock.On("GetStorageClasses", context.Background(), &s3.BaseRequest{}).Return(MockedGetStorageClasses()).
+		Once()
+	bkt := &s3.Bucket{Name: "gsnbkt"}
+	bucket, _ := MockedGetBucket()
+	s3ClientMock.On("GetBucket", context.Background(), bkt).Return(bucket, nil).
+		Times(5)
+
+	delBktCycle:= s3.DeleteLifecycleInput{}
+	delBktCycle.Bucket="gsnbkt"
+	delBktCycle.RuleID="rid"
+
+
+	fmt.Printf("test bucket is : %v\n", bucket)
+
+	s3ClientMock.On("DeleteBucketLifecycle", context.Background(), &delBktCycle).Return(&s3Resp, nil).
+		Times(5)
+
+
+
 	type fields struct {
 		s3Client      s3.S3Service
 		backendClient backend.BackendService
 	}
+	fields1:= fields{
+		s3ClientMock,
+		backEndMock,
+	}
 	type args struct {
 		request  *restful.Request
 		response *restful.Response
+	}
+	argsTest := args{
+		request:  rq,
+		response: rp,
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
 	}{
+		{"delbucketTest1",fields1,argsTest},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		tx.Run(tt.name, func(t *testing.T) {
 			s := &APIService{
 				s3Client:      tt.fields.s3Client,
 				backendClient: tt.fields.backendClient,
@@ -59,4 +108,26 @@ func TestAPIService_BucketLifecycleDelete(t *testing.T) {
 			s.BucketLifecycleDelete(tt.args.request, tt.args.response)
 		})
 	}
+	Result = rq.PathParameter("bucketName")
+	log.Logf("received request for delete bucket lifecycle: %s", Result1)
+}
+func TestAPIService_BucketLifecycleDelete(t *testing.T) {
+
+	tx = t
+	//========================================================
+
+	// setup service
+	ws := new(restful.WebService)
+	ws.Consumes(restful.MIME_XML)
+	ws.Route(ws.DELETE("/v1/s3/{bucketName}").To(BucketDelHandler))
+	restful.Add(ws)
+
+	// setup request + writer
+	bodyReader := strings.NewReader("<Sample><Value>gsnbkt</Value></Sample>")
+	httpRequest, _ := http.NewRequest("DELETE", "/v1/s3/gsnbkt/?lifecycle&ruleID=rid", bodyReader)
+	httpRequest.Header.Set("Content-Type", restful.MIME_XML)
+	httpWriter := httptest.NewRecorder()
+
+	// run
+	restful.DefaultContainer.ServeHTTP(httpWriter, httpRequest)
 }
