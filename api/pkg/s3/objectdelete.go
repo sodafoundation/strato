7ef1bf31de62152044792b257d1427c71c0a0454
YuZou <hopkings2005@hotmail.com>
2019-10-23 16:52:05 +0800

    make object delete function empty since this pr doesn't finish it.

--- api/pkg/s3/objectdelete.go
+++ api/pkg/s3/objectdelete.go
@@ -16,41 +16,7 @@ package s3
 
 import (
 	"github.com/emicklei/go-restful"
-	log "github.com/sirupsen/logrus"
 )
 
 func (s *APIService) ObjectDelete(request *restful.Request, response *restful.Response) {
-	//url := request.Request.URL
-	log.Infof("DELETE object, URL is %v", request.Request.URL.String())
-	/*bucketName := request.PathParameter("bucketName")
-	objectKey := request.PathParameter("objectKey")
-	if strings.HasSuffix(url.String(), "/") {
-		objectKey = objectKey + "/"
-	}
-	deleteInput := s3.DeleteObjectInput{Key: objectKey, Bucket: bucketName}
-
-	ctx := common.InitCtxWithAuthInfo(request)
-	objectInput := s3.GetObjectInput{Bucket: bucketName, Key: objectKey}
-	objectMD, _ := s.s3Client.GetObject(ctx, &objectInput)
-	if objectMD != nil {
-		client := getBackendByName(ctx, s, objectMD.Backend)
-		s3err := client.DELETE(&deleteInput, ctx)
-		if s3err.Code != ERR_OK {
-			response.WriteError(http.StatusInternalServerError, s3err.Error())
-			return
-		}
-		res, err := s.s3Client.DeleteObject(ctx, &deleteInput)
-		if err != nil {
-			log.Errorf("err is %v\n", err)
-			response.WriteError(http.StatusInternalServerError, err)
-			return
-		}
-		log.Infof("Delete object %s successfully.", objectKey)
-		response.WriteEntity(res)
-	} else {
-		log.Errorf("No such object")
-		return
-	}*/
-
-
 }
