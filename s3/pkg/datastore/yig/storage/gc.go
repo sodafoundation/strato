177a7683f6d148d1204d63b2ee5bf5b91887fa0d
root <zengshufang@huawei.com>
2019-12-03 20:31:12 +0800

    bug fix

--- s3/pkg/datastore/yig/storage/gc.go
+++ s3/pkg/datastore/yig/storage/gc.go
@@ -49,7 +49,7 @@ func (gm *GcMgr) Start() {
 				log.Infof("GcMgr is stopping.")
 				gm.wg.Done()
 				return
-			case <-time.After(time.Second * time.Duration(6000)):
+			case <-time.After(time.Second * time.Duration(gm.loopTime)):
 			}
 			var chs []<-chan *GcObjectResult
 			// get all the gc objects for this loop
