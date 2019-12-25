231545fdb2c24509a435e2fc587af24b9220aad9
root <314086186@qq.com>
2019-12-25 11:56:49 +0800

    fix issue#841

--- api/pkg/s3/bucketput.go
+++ api/pkg/s3/bucketput.go
@@ -90,7 +90,7 @@ func (s *APIService) BucketPut(request *restful.Request, response *restful.Respo
 
 	rsp, err := s.s3Client.CreateBucket(ctx, &bucket)
 	if HandleS3Error(response, request, err, rsp.GetErrorCode()) != nil {
-		log.Errorf("delete bucket[%s] failed, err=%v, errCode=%d\n", bucketName, err, rsp.ErrorCode)
+		log.Errorf("delete bucket[%s] failed, err=%v, errCode=%d\n", bucketName, err, rsp.GetErrorCode())
 		return
 	}
 
