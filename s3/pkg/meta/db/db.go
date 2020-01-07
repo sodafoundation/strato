0b95729e3579c70867bf98a14f5de2039378c3e8
nguptaopensds <ngopensds@gmail.com>
2020-01-07 17:34:52 +0530

    Fix for excluding versioning in Daito

--- s3/pkg/meta/db/db.go
+++ s3/pkg/meta/db/db.go
@@ -65,6 +65,6 @@ type DBAdapter interface {
 	DeleteGcobjRecord(ctx context.Context, o *Object, tx interface{}) (err error)
 	ListGcObjs(ctx context.Context, offset, limit int) ([]*Object, error)
 
-	UpdateBucketVersioning(ctx context.Context, bucketName string, versionStatus string) error
-	CreateBucketVersioning(ctx context.Context, bucketName string, versionStatus string) error
+	//UpdateBucketVersioning(ctx context.Context, bucketName string, versionStatus string) error
+	//CreateBucketVersioning(ctx context.Context, bucketName string, versionStatus string) error
 }
