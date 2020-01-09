217e8792fb2835880f2484980d07368ce618a438
rhsakarpos@gmail.com <rhsakarpos@gmail.com>
2020-01-09 14:59:19 +0530

    fix review comments

--- s3/initdb.sh
+++ s3/initdb.sh
@@ -5,17 +5,17 @@ succeed=false
 for i in 1 2 3 4 5
 do
     echo  "trying again, attempt " $i
-    mysql -h tidb -P 4000 -u root -e "create database if not exists s3 character set utf8;use s3;source ./s3.sql;"
+    mysql -h tidb -P 4000 -u root -e "create database if not exists s3 character set utf8;use s3;source /s3.sql;create database if not exists yig character set utf8;use yig;source /yig.sql;"
     if [ "$?" = "0" ]; then
         succeed=true
         break
     fi
-    sleep 5
-    echo  "sleeping to try again"
+    echo  "waiting 2 seconds, to try again"
+    sleep 2
 done
 if [ "${succeed}" = "true" ]; then
-    echo "create database succeed ..."
+    echo "create database succeeded..."
 else
-    echo "create database failed ..."
+    echo "create database failed..."
 fi
 ./s3
