55b5c5e297f03d0c2995d641c9ad22f43055d30f
Himanshu Varshney <himanshuvar@gmail.com>
2020-06-08 18:06:40 +0530

    Implement File Support InfraStructure
    * Add Docker configuration
    * Add Base Framework for File Support
    * Update docker-compose.yml

---
+++ file/pkg/service/service.go
@@ -0,0 +1,28 @@
+// Copyright 2020 The OpenSDS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	pb "github.com/opensds/multi-cloud/file/proto"
+	log "github.com/sirupsen/logrus"
+)
+
+type fileService struct {}
+
+func NewFileService() pb.FileHandler {
+
+	log.Infof("Init file service finished.\n")
+	return &fileService{}
+}
