1e1464194519a671b38cf50d2079c9edf8daf247
Himanshu Varshney <himanshuvar@gmail.com>
2020-09-27 21:49:27 +0530

    Bug-Fix: Added check for nil Fileshare return for Updation

--- file/pkg/service/service.go
+++ file/pkg/service/service.go
@@ -292,7 +292,10 @@ func (f *fileService) UpdateFileShare(ctx context.Context, in *pb.UpdateFileShar
 
 	fs, err := sd.UpdatefileShare(ctx, in)
 	if err != nil {
-		log.Errorf("Received error in creating file shares at backend %s", err)
+		log.Errorf("Received error in updating file shares at backend %s", err)
+		if fs == nil {
+			return err
+		}
 		fs.Fileshare.Status = utils.FileShareStateError
 	} else {
 		fs.Fileshare.Status = utils.FileShareStateUpdating
