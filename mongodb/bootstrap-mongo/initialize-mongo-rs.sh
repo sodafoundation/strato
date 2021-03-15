767fb8cb71c266a595235576de5a4bc293ba9107
rajat verma <rajatsoda009@gmail.com>
2021-03-15 12:30:11 +0530

    update license stmt

--- mongodb/bootstrap-mongo/initialize-mongo-rs.sh
+++ mongodb/bootstrap-mongo/initialize-mongo-rs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2021 The SODA Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 echo "Executing initialize-mongo-rs.sh" >> initialize-mongo-rs-log.txt
 mongo --eval "mongodb = ['$MONGODB_0_SERVICE_SERVICE_HOST:$MONGODB_0_SERVICE_SERVICE_PORT', '$MONGODB_1_SERVICE_SERVICE_HOST:$MONGODB_1_SERVICE_SERVICE_PORT', '$MONGODB_2_SERVICE_SERVICE_HOST:$MONGODB_2_SERVICE_SERVICE_PORT']" --shell << EOL
 cfg = {
