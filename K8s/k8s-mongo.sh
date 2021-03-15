93bbcdec02b2ec49f698c3bde1f7e384c35e8491
rajat verma <rajatsoda009@gmail.com>
2021-03-15 11:36:17 +0530

    add namespace for deploy and delete multi-cloud services

--- K8s/k8s-mongo.sh
+++ K8s/k8s-mongo.sh
@@ -1,14 +1,15 @@
-kubectl apply -f mongo-pv-0.yaml
-kubectl apply -f mongo-pv-1.yaml
-kubectl apply -f mongo-pv-2.yaml
+kubectl apply -f namespace.yaml
 
-kubectl apply -f mongo-pvc-0.yaml
-kubectl apply -f mongo-pvc-1.yaml
-kubectl apply -f mongo-pvc-2.yaml
+kubectl apply -f mongo-pv-0.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pv-1.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pv-2.yaml -n soda-multi-cloud
+ 
+kubectl apply -f mongo-pvc-0.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pvc-1.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pvc-2.yaml -n soda-multi-cloud
 
-
-kubectl apply -f mongo-service-0.yaml
-kubectl apply -f mongo-service-1.yaml
-kubectl apply -f mongo-service-2.yaml
+kubectl apply -f mongo-service-0.yaml -n soda-multi-cloud
+kubectl apply -f mongo-service-1.yaml -n soda-multi-cloud
+kubectl apply -f mongo-service-2.yaml -n soda-multi-cloud
 
 
