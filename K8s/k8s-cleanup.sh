814118e682a0a6b8312ef20dc904e5103d25c347
rajat-soda <65751939+rajat-soda@users.noreply.github.com>
2021-03-16 19:45:04 +0530

    Deploy multi-cloud services in soda-multi-cloud namespace (#1211)
    * add namespace for deploy and delete multi-cloud services
    
    * include k8s delete namespace
    
    * update license stmt
    
    * resolve review comments
    
    * resolve review comments

--- K8s/k8s-cleanup.sh
+++ K8s/k8s-cleanup.sh
@@ -1,44 +1,50 @@
 #Delete the services
-kubectl delete service  zookeeper 
-kubectl delete service  tidb 
-kubectl delete service  redis 
-#kubectl delete service  datastore 
-kubectl delete service  kafka 
-		
-kubectl delete service  api 
-kubectl delete service  s3api 
-kubectl delete service  s3 
-kubectl delete service  backend 
-kubectl delete service  block 
-kubectl delete service  file 
-kubectl delete service  datamover 
-kubectl delete service  dataflow 
-kubectl delete service  mongo
-
-kubectl delete statefulset mongo 
+kubectl delete service  zookeeper -n soda-multi-cloud
+kubectl delete service  tidb -n soda-multi-cloud
+kubectl delete service  redis -n soda-multi-cloud
+kubectl delete service  kafka -n soda-multi-cloud
+kubectl delete service  api -n soda-multi-cloud
+kubectl delete service  s3api -n soda-multi-cloud
+kubectl delete service  mongodb-0-service -n soda-multi-cloud
+kubectl delete service  mongodb-1-service -n soda-multi-cloud
+kubectl delete service  mongodb-2-service -n soda-multi-cloud
 
 # Delete the deployments
+kubectl delete deployment zookeeper -n soda-multi-cloud
+kubectl delete deployment redis -n soda-multi-cloud
+kubectl delete deployment tidb -n soda-multi-cloud
+kubectl delete deployment kafka-svc -n soda-multi-cloud
+kubectl delete deployment block -n soda-multi-cloud
+kubectl delete deployment file -n soda-multi-cloud
+kubectl delete deployment backend -n soda-multi-cloud
+kubectl delete deployment s3api -n soda-multi-cloud
+kubectl delete deployment s3 -n soda-multi-cloud
+kubectl delete deployment api -n soda-multi-cloud
+kubectl delete deployment datamover -n soda-multi-cloud
+kubectl delete deployment dataflow -n soda-multi-cloud
+kubectl delete deployment  mongodb-0 -n soda-multi-cloud
+kubectl delete deployment  mongodb-1 -n soda-multi-cloud
+kubectl delete deployment  mongodb-2 -n soda-multi-cloud
 
-kubectl delete deployment zookeeper 
-kubectl delete deployment redis 
-kubectl delete deployment tidb 
-kubectl delete deployment datastore 
-kubectl delete deployment kafka-svc 
-			   
-kubectl delete deployment block 
-kubectl delete deployment file 
-kubectl delete deployment backend 
-kubectl delete deployment s3api 
-kubectl delete deployment s3 
-kubectl delete deployment api 
-kubectl delete deployment datamover 
-kubectl delete deployment dataflow 
+# Delete config maps
+kubectl delete configmap multicloud-config -n soda-multi-cloud
+kubectl delete configmap tidb-config -n soda-multi-cloud
+kubectl delete configmap s3-config -n soda-multi-cloud
+kubectl delete configmap tidb-sql -n soda-multi-cloud
+kubectl delete configmap s3-sql -n soda-multi-cloud
+kubectl delete configmap yig-sql -n soda-multi-cloud
 
-kubectl delete configmap multicloud-config 
-kubectl delete configmap tidb-config 
-kubectl delete configmap s3-config  
+# Delete pv, pvc
+kubectl patch pvc mongo-0-pv-claim -p '{"metadata":{"finalizers": []}}' --type=merge -n soda-multi-cloud
+kubectl patch pvc mongo-1-pv-claim -p '{"metadata":{"finalizers": []}}' --type=merge -n soda-multi-cloud
+kubectl patch pvc mongo-2-pv-claim -p '{"metadata":{"finalizers": []}}' --type=merge -n soda-multi-cloud
  
-kubectl delete configmap tidb-sql 
-kubectl delete configmap s3-sql 
-kubectl delete configmap yig-sql 
+kubectl delete pvc mongo-0-pv-claim -n soda-multi-cloud
+kubectl delete pvc mongo-1-pv-claim -n soda-multi-cloud
+kubectl delete pvc mongo-2-pv-claim -n soda-multi-cloud
+
+kubectl delete pv mongo-0-pv-volume -n soda-multi-cloud
+kubectl delete pv mongo-1-pv-volume -n soda-multi-cloud
+kubectl delete pv mongo-2-pv-volume -n soda-multi-cloud
+
 
--- K8s/k8s-deploy.sh
+++ K8s/k8s-deploy.sh
@@ -1,45 +1,36 @@
 # Run this file from multi-cloud folder.
-
-kubectl apply -f rbac.yaml
+kubectl apply -f rbac.yaml -n soda-multi-cloud
 
 #1. Create all the config Maps from files. 
-kubectl create configmap multicloud-config --from-file=../examples/multi-cloud.conf 
-kubectl create configmap tidb-config --from-file=../s3/tidbconf/tidb.toml 
-kubectl create configmap s3-config --from-file=../s3/pkg/conf/s3.toml 
-
-kubectl create configmap tidb-sql --from-file=../s3/pkg/conf/tidb.sql 
-kubectl create configmap s3-sql --from-file=../s3/pkg/conf/tidb.sql 
-kubectl create configmap yig-sql --from-file=../s3/pkg/datastore/yig/conf/yig.sql 
+kubectl create configmap multicloud-config --from-file=../examples/multi-cloud.conf  -n soda-multi-cloud
+kubectl create configmap tidb-config --from-file=../s3/tidbconf/tidb.toml -n soda-multi-cloud
+kubectl create configmap s3-config --from-file=../s3/pkg/conf/s3.toml -n soda-multi-cloud
+kubectl create configmap tidb-sql --from-file=../s3/pkg/conf/tidb.sql -n soda-multi-cloud
+kubectl create configmap s3-sql --from-file=../s3/pkg/conf/tidb.sql -n soda-multi-cloud
+kubectl create configmap yig-sql --from-file=../s3/pkg/datastore/yig/conf/yig.sql -n soda-multi-cloud
 
 #2. Create all pods with no dependencies. 
-kubectl apply -f zookeeper-deployment.yaml 
-kubectl apply -f redis-deployment.yaml 
-kubectl apply -f tidb-deployment.yaml 
-#kubectl apply -f mongo-deployment.yaml 
-kubectl apply -f kafka-deployment.yaml 
+kubectl apply -f zookeeper-deployment.yaml -n soda-multi-cloud
+kubectl apply -f redis-deployment.yaml -n soda-multi-cloud
+kubectl apply -f tidb-deployment.yaml -n soda-multi-cloud
+kubectl apply -f kafka-deployment.yaml -n soda-multi-cloud
 
-kubectl apply -f block-deployment.yaml 
-kubectl apply -f file-deployment.yaml 
-kubectl apply -f backend-deployment.yaml 
-kubectl apply -f s3api-deployment.yaml 
-kubectl apply -f api-deployment.yaml 
-kubectl apply -f datamover-deployment.yaml 
-kubectl apply -f dataflow-deployment.yaml 
-kubectl apply -f s3-deployment.yaml 
+kubectl apply -f block-deployment.yaml -n soda-multi-cloud
+kubectl apply -f file-deployment.yaml -n soda-multi-cloud
+kubectl apply -f backend-deployment.yaml -n soda-multi-cloud
+kubectl apply -f s3api-deployment.yaml -n soda-multi-cloud
+kubectl apply -f api-deployment.yaml -n soda-multi-cloud
+kubectl apply -f datamover-deployment.yaml -n soda-multi-cloud
+kubectl apply -f dataflow-deployment.yaml -n soda-multi-cloud
+kubectl apply -f s3-deployment.yaml -n soda-multi-cloud
 
 #4 Expose the services
-kubectl apply -f zookeeper-service.yaml 
-#kubectl apply -f mongo-service.yaml 
-kubectl apply -f tidb-service.yaml 
-kubectl apply -f redis-service.yaml 
-kubectl apply -f kafka-service.yaml 
-
-kubectl apply -f api-service.yaml 
-kubectl apply -f s3api-service.yaml 
-#kubectl apply -f mongo-headless-service.yaml
-#kubectl apply -f mongo-statefulset.yaml
-
+kubectl apply -f zookeeper-service.yaml -n soda-multi-cloud
+kubectl apply -f tidb-service.yaml -n soda-multi-cloud
+kubectl apply -f redis-service.yaml -n soda-multi-cloud
+kubectl apply -f kafka-service.yaml -n soda-multi-cloud
+kubectl apply -f api-service.yaml -n soda-multi-cloud
+kubectl apply -f s3api-service.yaml -n soda-multi-cloud
 
 #5. Get all the deployed objects
-
-kubectl get all 
+kubectl get all -n soda-multi-cloud
