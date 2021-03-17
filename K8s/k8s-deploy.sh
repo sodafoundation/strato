20b1a2388b26ee730d9515c8b7f66fc21d12d046
Rajat Verma <rajatsoda009@gmail.com>
2021-03-17 15:27:48 +0530

    Add namespaces and fix s3 initdb

--- K8s/k8s-deploy.sh
+++ K8s/k8s-deploy.sh
@@ -1,5 +1,5 @@
 # Run this file from multi-cloud folder.
-kubectl apply -f rbac.yaml -n soda-multi-cloud
+kubectl apply -f rbac.yaml 
 
 #1. Create all the config Maps from files. 
 kubectl create configmap multicloud-config --from-file=../examples/multi-cloud.conf  -n soda-multi-cloud
@@ -10,27 +10,27 @@ kubectl create configmap s3-sql --from-file=../s3/pkg/conf/tidb.sql -n soda-mult
 kubectl create configmap yig-sql --from-file=../s3/pkg/datastore/yig/conf/yig.sql -n soda-multi-cloud
 
 #2. Create all pods with no dependencies. 
-kubectl apply -f zookeeper-deployment.yaml -n soda-multi-cloud
-kubectl apply -f redis-deployment.yaml -n soda-multi-cloud
-kubectl apply -f tidb-deployment.yaml -n soda-multi-cloud
-kubectl apply -f kafka-deployment.yaml -n soda-multi-cloud
+kubectl apply -f zookeeper-deployment.yaml 
+kubectl apply -f redis-deployment.yaml 
+kubectl apply -f tidb-deployment.yaml 
+kubectl apply -f kafka-deployment.yaml 
 
-kubectl apply -f block-deployment.yaml -n soda-multi-cloud
-kubectl apply -f file-deployment.yaml -n soda-multi-cloud
-kubectl apply -f backend-deployment.yaml -n soda-multi-cloud
-kubectl apply -f s3api-deployment.yaml -n soda-multi-cloud
-kubectl apply -f api-deployment.yaml -n soda-multi-cloud
-kubectl apply -f datamover-deployment.yaml -n soda-multi-cloud
-kubectl apply -f dataflow-deployment.yaml -n soda-multi-cloud
-kubectl apply -f s3-deployment.yaml -n soda-multi-cloud
+kubectl apply -f block-deployment.yaml 
+kubectl apply -f file-deployment.yaml 
+kubectl apply -f backend-deployment.yaml 
+kubectl apply -f s3api-deployment.yaml 
+kubectl apply -f api-deployment.yaml 
+kubectl apply -f datamover-deployment.yaml 
+kubectl apply -f dataflow-deployment.yaml 
+kubectl apply -f s3-deployment.yaml 
 
 #4 Expose the services
-kubectl apply -f zookeeper-service.yaml -n soda-multi-cloud
-kubectl apply -f tidb-service.yaml -n soda-multi-cloud
-kubectl apply -f redis-service.yaml -n soda-multi-cloud
-kubectl apply -f kafka-service.yaml -n soda-multi-cloud
-kubectl apply -f api-service.yaml -n soda-multi-cloud
-kubectl apply -f s3api-service.yaml -n soda-multi-cloud
+kubectl apply -f zookeeper-service.yaml 
+kubectl apply -f tidb-service.yaml 
+kubectl apply -f redis-service.yaml 
+kubectl apply -f kafka-service.yaml 
+kubectl apply -f api-service.yaml 
+kubectl apply -f s3api-service.yaml 
 
 #5. Get all the deployed objects
 kubectl get all -n soda-multi-cloud
