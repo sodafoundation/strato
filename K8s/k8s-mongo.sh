17b954fb414baa34b5739e5a8bc38e244ee6ec0c
rajat-soda <65751939+rajat-soda@users.noreply.github.com>
2021-03-18 12:25:49 +0530

    Add namespaces and fix s3 initdb (#1214)
    * Add namespaces and fix s3 initdb
    
    * remove unwanted files
    
    * Use latest images for deploying the K8s pods
    
    * cleanup

--- K8s/k8s-mongo.sh
+++ K8s/k8s-mongo.sh
@@ -1,15 +1,15 @@
-kubectl create namespace soda-multi-cloud
+#kubectl create namespace soda-multi-cloud
 
-kubectl apply -f mongo-pv-0.yaml -n soda-multi-cloud
-kubectl apply -f mongo-pv-1.yaml -n soda-multi-cloud
-kubectl apply -f mongo-pv-2.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pv-0.yaml
+kubectl apply -f mongo-pv-1.yaml
+kubectl apply -f mongo-pv-2.yaml
  
-kubectl apply -f mongo-pvc-0.yaml -n soda-multi-cloud
-kubectl apply -f mongo-pvc-1.yaml -n soda-multi-cloud
-kubectl apply -f mongo-pvc-2.yaml -n soda-multi-cloud
+kubectl apply -f mongo-pvc-0.yaml
+kubectl apply -f mongo-pvc-1.yaml
+kubectl apply -f mongo-pvc-2.yaml
 
-kubectl apply -f mongo-service-0.yaml -n soda-multi-cloud
-kubectl apply -f mongo-service-1.yaml -n soda-multi-cloud
-kubectl apply -f mongo-service-2.yaml -n soda-multi-cloud
+kubectl apply -f mongo-service-0.yaml
+kubectl apply -f mongo-service-1.yaml
+kubectl apply -f mongo-service-2.yaml
 
-
+kubectl get pods -n soda-multi-cloud
