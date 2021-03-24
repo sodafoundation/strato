1228464e83038fbdea531faf68c27c35d45a27b3
rajat verma <rajatsoda009@gmail.com>
2021-03-24 15:48:02 +0530

    implement review comments

--- K8s/soda_scale_service.sh
+++ K8s/soda_scale_service.sh
@@ -5,10 +5,13 @@ service_name="$2"
 if [ -z "$service_name" ] || [ -z "$no_of_pods" ]
 then
         echo "Usage : ./soda_scale_service <service_name> <number_of_instances>"
-else
-        echo "START :: Scaling Service " $2 to $1 "instances.."
-        cmd="kubectl scale --replicas=$2  deployment/$1 -n soda-multi-cloud"
-        echo "EXECUTING ... $cmd"
-        bash -c "$cmd"
-        echo "COMPLETED :: Scaling Service " $2 to $1 "instances..."
-fi
+        exit 1
+
+echo "START :: Scaling Service " $2 to $1 "instances.."
+cmd="kubectl scale --replicas=$2  deployment/$1 -n soda-multi-cloud"
+echo "EXECUTING ... $cmd"
+bash -c "$cmd"
+status=$?
+[ $status -eq 0 ] && echo "COMPLETED :: Scaling Service " $2 to $1 "instances..."
+exit 0
+
