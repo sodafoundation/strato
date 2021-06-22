21672635ec17a69ffed56589b2ab1a829a753df0
rajat-soda <rajatsoda009@gmail.com>
2021-06-22 07:10:09 +0530

    go fmt

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/emicklei/go-restful"
 	"github.com/micro/go-micro/v2/web"
-	log "github.com/sirupsen/logrus"
 
         "github.com/opensds/multi-cloud/api/pkg/aksk"
 	"github.com/opensds/multi-cloud/api/pkg/backend"
@@ -32,6 +31,8 @@ import (
 	"github.com/opensds/multi-cloud/api/pkg/filters/signature/signer"
 	"github.com/opensds/multi-cloud/api/pkg/s3"
 	"github.com/opensds/multi-cloud/api/pkg/utils/obs"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const (
