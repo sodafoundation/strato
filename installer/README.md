c5ceadcd01d2e71c680bdeebc3edda2e47dee36c
Devansh Jain <devanshjain293@gmail.com>
2022-11-28 15:15:44 +0530

    added go version check in install_dependencies.sh, minor changes in README

--- installer/README.md
+++ installer/README.md
@@ -1,31 +1,26 @@
-# Multicloud Installation Guide
+# Multi-cloud Installation Guide
 
 ## Ansible Installer
 * Supported OS: **Ubuntu 18.04, 20.04**
-* Prerequisite: **Python 3.6 or above** should be installed
+* Prerequisite: **Python 3.6+** and **Go 1.17+**  should be installed
 
 **Note**: Ensure root user access while performing the installation.
 
-### Install required packages
+### Install Steps
 ```bash
 apt-get update && apt-get install -y git curl wget libltdl7 libseccomp2 libffi-dev
-```
-
-### Clone the Multicloud repository
-```bash
 git clone https://github.com/sodafoundation/multi-cloud.git
-```
-### Install required dependencies for Multicloud (Docker, Ansible, Go)
-```bash
 cd multi-cloud/installer
 chmod +x install_dependencies.sh && . install_dependencies.sh
 ```
+
 ### Set Host IP
 ```bash
 cd ansible
 export HOST_IP=<your_host_ip>
 ```
-### Run the Ansible Playbook to install Multicloud
+
+### Run the Ansible Playbook to install Multi-cloud
 ```bash
 ansible-playbook site.yml -i local.hosts
 ```
--- installer/install_dependencies.sh
+++ installer/install_dependencies.sh
@@ -22,7 +22,7 @@ echo Installing Python dependencies
 apt-get install -y python3-pip
 
 # Install ansible if not present
-if [ "`which ansible`" != ""  ]; then
+if [ "`which ansible`" != "" ]; then
     echo ansible already installed, skipping.
 else
     echo Installing ansible
@@ -30,7 +30,7 @@ else
 fi
 
 # Install docker if not present
-if [ "`which docker`" != ""  ]; then
+if [ "`which docker`" != "" ]; then
     echo Docker already installed, skipping.
 else
     echo Installing docker
@@ -38,10 +38,23 @@ else
 fi
 
 # Install Go if not present
-if [ "`which go`" != ""  ]; then
-    echo Go already installed, skipping.
+if [ "`which go`" != "" ]; then
+    IFS=' '
+    v=`go version | { read _ _ v _; echo ${v#go}; }`
+    IFS='.'
+    read -ra v <<< "$v"
+    if (( ${v[0]} == 1 && ${v[1]} >= 17 )); then
+        echo Go 1.17+ already installed, skipping.
+    else
+        echo Removing existing Go installation
+        rm -rf /usr/local/go
+        echo Installing Go 1.17.9
+        wget https://storage.googleapis.com/golang/go1.17.9.linux-amd64.tar.gz
+        tar -C /usr/local -xzf go1.17.9.linux-amd64.tar.gz
+    fi
+    unset IFS v
 else
-    echo Installing Go
+    echo Installing Go 1.17.9
     wget https://storage.googleapis.com/golang/go1.17.9.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.17.9.linux-amd64.tar.gz
 fi
