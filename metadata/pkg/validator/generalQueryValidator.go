1b9380200bfdb971e43a8b6daf71653249d884aa
Joseph Jacob <josephjacob1998@gmail.com>
2023-01-04 12:04:33 +0530

    [metadata manager m8s] [Validator] validate size request params and operators

--- metadata/pkg/validator/generalQueryValidator.go
+++ metadata/pkg/validator/generalQueryValidator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"github.com/opensds/multi-cloud/metadata/pkg/constants"
 	pb "github.com/opensds/multi-cloud/metadata/proto"
 )
 
@@ -10,9 +11,38 @@ type GeneralQueryValidator struct {
 func (v *GeneralQueryValidator) isValidQuery(in *pb.ListMetadataRequest) (bool, error) {
 	// TODO: validate backend name if present is valid according to backend naming conventions
 
-	// TODO: validate size of bucket is valid
+	// validate size of object and operator is valid
+	okie, err := isSizeParamsValid(in.SizeOfBucketInBytes, in.BucketSizeOperator)
+	if !okie {
+		return okie, err
+	}
 
-	// TODO: validate size of object is valid
+	// validate size of object and operator is valid
+	okie, err = isSizeParamsValid(in.SizeOfObjectInBytes, in.ObjectSizeOperator)
+
+	return okie, err
+}
+
+func isSizeParamsValid(sizeInBytes int32, operator string) (bool, error) {
+	switch operator {
+	case constants.EQUAL_OPERATOR:
+
+	case constants.LESS_THAN_EQUAL_OPERATOR:
+
+	case constants.LESS_THAN_OPERATOR:
+
+	case constants.GREATER_THAN_EQUAL_OPERATOR:
+
+	case constants.GREATER_THAN_OPERATOR:
+		// operator is valid
+		break
+	default:
+		return false, &ValidationError{errMsg: "Operator for size should be  lte, gt, eq, gte or lt"}
+	}
+
+	if sizeInBytes < constants.ZERO {
+		return false, &ValidationError{errMsg: "Size should be always positive."}
+	}
 
 	return true, nil
 }
